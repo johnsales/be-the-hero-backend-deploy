e0a0f1f2da7ce31b77be8013ea50138296ac4e1e
John Santiago <jmsvsmorone@hotmail.com>
2020-03-30 21:00:07 -0300

    test

---
+++ backend/tests/integration/ong.spec.js
@@ -0,0 +1,30 @@
+const request = require('supertest');
+const app = require('../../src/app');
+const connection = require('../../src/database/connection');
+
+describe('ONG',() => {
+
+    beforeEach(async () =>{
+        await connection.migrate.rollback();
+        await connection.migrate.latest();
+    });
+
+    afterAll(async () => {
+        await connection.destroy();
+    });
+
+    it('sould be able to create a new ONG', async () =>{
+        const response = await request(app)
+        .post('/ongs')
+        .send({
+            name:"nome",
+            email:"cpn@b.com",
+            whatsapp:"8499999999",
+            city:"natal",
+            uf:"SC"
+        });
+
+        expect(response.body).toHaveProperty('id');
+        expect(response.body.id).toHaveLength(8);
+    });
+});
